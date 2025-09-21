fb39a8428b3a974ebca8e14be2c6d78eb2fa28a0
kchapl <kchapl@users.noreply.github.com>
2025-09-21 20:56:36 +0000

    chore: Add PostgreSQL support

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_basicauth import BasicAuth
+from flask_sqlalchemy import SQLAlchemy
+import click
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
-app.config['BASIC_AUTH_USERNAME'] = 'admin'
-app.config['BASIC_AUTH_PASSWORD'] = 'n1vl3k'
+# Database configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-basic_auth = BasicAuth(app)
+class User(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+
+class MyAuth(BasicAuth):
+    def check_credentials(self, username, password):
+        user = User.query.filter_by(user_name=username).first()
+        if user and user.password == password:
+            return True
+        return False
+
+basic_auth = MyAuth(app)
 
 @app.route('/')
 @basic_auth.required
@@ -17,6 +37,23 @@ def home():
 @basic_auth.required
 def about():
     return '<h1>About</h1><p>This is a simple Flask web application.</p>'
+
+@app.cli.command("init-db")
+def init_db():
+    """Initialize the database and create/update a default user."""
+    db.create_all()
+    username = os.environ.get('DEFAULT_USERNAME', 'admin')
+    password = os.environ.get('DEFAULT_PASSWORD', 'admin')
+    user = User.query.filter_by(user_name=username).first()
+    if user:
+        user.password = password
+        print(f"Default user '{username}' password updated.")
+    else:
+        user = User(user_name=username, password=password)
+        db.session.add(user)
+        print(f"Default user '{username}' created.")
+    db.session.commit()
+    print("Database initialized.")
 
 if __name__ == '__main__':
     app.run(debug=True)
