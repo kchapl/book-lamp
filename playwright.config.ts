f9409e58d73ec63eb40f20581876bf66920a5772
K <kchapl@users.noreply.github.com>
2025-10-22 20:31:22 +0100

    ci: Set retries to 0 for consistent test behavior

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     globalSetup: require.resolve('./tests-e2e/global.setup'),
     fullyParallel: true,
     forbidOnly: !!process.env.CI,
-    retries: process.env.CI ? 2 : 0,
+    retries: 0,
     workers: process.env.CI ? 2 : undefined,
     reporter: [['html', { open: 'never' }], ['list']],
     use: {
