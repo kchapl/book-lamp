45d1536a255bd3d8eb83488d282a4c2f4f7b82b7
kchapl <kchapl@users.noreply.github.com>
2026-01-30 20:17:43 +0000

    feat: Configure Playwright's GitHub reporter conditionally in config and remove it from CI workflow command.

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,7 +11,9 @@ export default defineConfig({
     forbidOnly: !!process.env.CI,
     retries: process.env.CI ? 2 : 0,
     workers: 1,
-    reporter: [['html', { open: 'never' }], ['list']],
+    reporter: process.env.CI
+        ? [['github'], ['html', { open: 'never' }], ['list']]
+        : [['html', { open: 'never' }], ['list']],
     use: {
         baseURL: BASE_URL,
         trace: 'on-first-retry',
